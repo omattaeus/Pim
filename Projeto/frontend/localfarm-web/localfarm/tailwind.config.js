7a22c0a82d6c9865b51b43209a5c51c2e5c25b90
Mateus Gomes <127147447+omattaeus@users.noreply.github.com>
2024-11-02 14:32:03 -0300

    feat: criando tela menu inicial

--- Projeto/frontend/localfarm-web/localfarm/tailwind.config.js
+++ Projeto/frontend/localfarm-web/localfarm/tailwind.config.js
@@ -15,6 +15,17 @@ module.exports = {
         'custom-brown': '#50413E',
         'custom-brown-20': 'rgba(80, 65, 62, 0.20)',
         'custom-orange-20': 'rgba(194, 130, 58, 0.20)',
+        'custom-green-93': 'rgba(107, 155, 114, 0.93)',
+        'custom-pink-87': 'rgba(255, 245, 245, 0.87)',
+        'text-green': '#405743',
+        'custom-green-95': 'rgba(95, 114, 97, 0.95)',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      lineHeight: {
+        'tight-24': '24px',
+        'tight-20': '20px',
       },
     },
   },
